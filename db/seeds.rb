4c744dee70f86a3f47b0de5f9568279f12ddaa62
Anna Bothe <annabothe0@gmail.com>
2023-11-15 16:48:38 +0100

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,22 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+puts 'Creating restaurant...'
+pipa = Restaurant.new(name: "Pipa", address: "Morsstraat 60", phone_number: "+3106887726", category: "italian")
+pipa.save!
+
+puts 'Creating restaurant...'
+el_gaucho = Restaurant.new(name: "El Gaucho", address: "Morsstraat 12", phone_number: "+31034875466", category: "french")
+el_gaucho.save!
+
+puts 'Creating restaurant...'
+ramen_king = Restaurant.new(name: "Ramen King", address: "Langloopstraat 2", phone_number: "062784726", category: "japanese")
+ramen_king.save!
+
+puts 'Creating restaurant...'
+fry_guy = Restaurant.new(name: "Fry Guy", address: "Sportlaan 156", phone_number: "(0035) 345872", category: "belgian")
+fry_guy.save!
+
+puts 'Creating restaurant...'
+ducky = Restaurant.new(name: "Ducky", address: "Lange Mors 48", phone_number: "887726635", category: "chinese")
+ducky.save!
